fafba73c125131866bc7b305fc5dcb5b8d387e39
dac <dac@dypm.org>
2023-01-06 16:22:08 +0530

    message

---
+++ MyApp/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MyApp.Models;
+
+namespace MyApp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Login()
+    {
+        return View();
+    }
+    public IActionResult ErrorPage()
+    {
+        return View();
+    }
+
+    public IActionResult Validate(string email, string password)
+    {
+        if (email == "sidd.bhatt@gmail.com" && password == "smile")
+        {
+            return Redirect("/home/Seat");
+        }
+        else
+        {
+            return Redirect("/home/ErrorPage");
+        }
+    }
+    public IActionResult Seat()
+    {
+        return View();
+    }
+    
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
